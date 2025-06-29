a64b381249c5526a4d3344a009dfc12e78432d21
Piyush75812 <piyushkumar63072@gmail.com>
2025-06-29 16:55:03 +0530

    Add files via upload

---
+++ bootcamp21c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    for(int row=0;row<=4;row++)
+    {
+        for(int col=1;col<=5-row-1;col++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+    }
+    
+    
+}
